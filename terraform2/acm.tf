7edf1a6febd23ab56c708f495cdca681d5c226d1
sowjiarha <sowjiarhana@gmail.com>
2023-05-01 10:42:53 +0530

    adding acm.tf

--- terraform2/acm.tf
+++ terraform2/acm.tf
@@ -1,4 +1,4 @@
-#finding a certificate issued by ACM (not imported)
+#ffinding a certificate issued by ACM (not imported)
 data "aws_acm_cdrtificate" "amazon_issued" {
   domain = var.domainName
   types  = ["AMAZON_ISSUED"]
